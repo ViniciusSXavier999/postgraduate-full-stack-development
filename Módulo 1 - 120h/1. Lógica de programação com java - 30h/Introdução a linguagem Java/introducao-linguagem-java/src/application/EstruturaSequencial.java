ea70aaf61074176033d9a7e7455d715b6b0ece29
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-22 11:30:52 -0300

    Estrutura sequencial

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/introducao-linguagem-java/src/application/EstruturaSequencial.java
@@ -0,0 +1,20 @@
+package application;
+
+public class EstruturaSequencial {
+
+	public static void main(String[] args) {
+		
+		// O computador começa executando aqui reservando um espaço na memória.
+		double base, altura, area;
+		
+		base = 10;
+		altura = 8;
+		
+		
+		area = (base * altura) / 2;
+		
+		System.out.println(area);
+
+	}
+
+}
