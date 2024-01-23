ae13aea75e86a3b0e04ebabca40dac33d5400b90
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-23 09:51:19 -0300

    Estrutura condicional simples

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/estruturas-condicionais/src/application/CondicionalSimples.java
@@ -0,0 +1,15 @@
+package application;
+
+public class CondicionalSimples {
+
+	public static void main(String[] args) {
+		
+		int nota = 6;
+		
+		if (nota >= 5 ) {
+			System.out.println("Aprovado!!");
+		}
+
+	}
+
+}
