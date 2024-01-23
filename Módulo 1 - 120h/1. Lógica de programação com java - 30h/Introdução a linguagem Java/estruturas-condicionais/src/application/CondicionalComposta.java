43d668c9f0367122436c4d7770ab072fd1789c7b
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-23 10:06:08 -0300

    Condicional Composta

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/estruturas-condicionais/src/application/CondicionalComposta.java
@@ -0,0 +1,19 @@
+package application;
+
+public class CondicionalComposta {
+	
+	public static void main (String[] args) {
+		
+		int nota = 6 ;
+		
+		if (nota >= 5) {
+			System.out.println("Aprovado!!");
+		} 
+		
+		else {
+			System.out.println("Reprovado!!");
+		}
+		
+	}
+
+}
