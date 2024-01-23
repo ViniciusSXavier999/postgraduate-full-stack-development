d42f225624cc29a325f51938381a1511dfabd227
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-23 10:42:27 -0300

    Estrutura condicional aninhada

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/estruturas-condicionais/src/application/CondicionalAninhada.java
@@ -0,0 +1,23 @@
+package application;
+
+public class CondicionalAninhada {
+
+	public static void main(String[] args) {
+		
+		int nota = 4;
+		
+		if(nota >= 5) {
+			System.out.println("Aprovado!!");
+		}
+		
+		else if (nota >= 3 && nota < 5) {
+			System.out.println("Exame!!");
+		}
+		
+		else {
+			System.out.println("Reprovado!!");
+		}
+
+	}
+
+}
