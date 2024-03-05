7856219ec08c44a11c7e7ac7bf757099b02a0a9c
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-03-05 18:37:33 -0300

    Ordenando os conteúdos aprendidos e iniciando Vetores e matrizes

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/03-estruturas-repetição/src/application/Metodo.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Metodo {
+	
+	public static int soma (int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int total =	Metodo.soma(50, 200);
+		
+		System.out.println(total);
+	}
+
+}
