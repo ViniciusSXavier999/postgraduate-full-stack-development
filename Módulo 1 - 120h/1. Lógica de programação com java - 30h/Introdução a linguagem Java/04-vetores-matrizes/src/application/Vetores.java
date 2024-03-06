21fcfa9d1a46e37d7c6c591385a143511a40a61a
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-03-05 22:04:30 -0300

    Vetores

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/04-vetores-matrizes/src/application/Vetores.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		
+		int[] dados = new int[5];
+		
+		dados[2] = 99;
+		dados[3] = 5;
+		dados[0] = 11;
+		
+		
+		System.out.println(dados[0]);
+
+	}
+
+}
