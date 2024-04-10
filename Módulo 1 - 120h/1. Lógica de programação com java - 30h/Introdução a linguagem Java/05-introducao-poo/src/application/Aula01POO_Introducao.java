d4d283d302e92a60bc3c6d40e40bbc3223722ba4
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-04-10 00:50:25 -0300

    Introdução a programação orientada a objetos

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/05-introducao-poo/src/application/Aula01POO_Introducao.java
@@ -0,0 +1,32 @@
+package application;
+
+public class Aula01POO_Introducao {
+
+	// classe principal
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+	// atributos
+	String cor;
+	String marca;
+	
+	// métodos
+	public void andar() {
+		
+	}
+	
+	public void frear() {
+		
+	}
+	
+	public void parar() {
+		
+	}
+	
+	
+
+}
