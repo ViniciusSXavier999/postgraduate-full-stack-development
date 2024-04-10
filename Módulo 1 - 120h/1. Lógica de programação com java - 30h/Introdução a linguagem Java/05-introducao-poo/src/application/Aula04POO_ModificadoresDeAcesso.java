811bc4f67707f9623bc5b8e1eb70dfa0110f4090
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-04-10 14:30:27 -0300

    Modificadores de acesso

---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/05-introducao-poo/src/application/Aula04POO_ModificadoresDeAcesso.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Aula04POO_ModificadoresDeAcesso {
+	
+	// ESSAS SÃO AS CARACTERISTICAS DO NOSSO OBJETO, O QUE CHAMAMOS DE ATRIBUTOS.
+	
+			public String nome;
+			public String cor;
+			public int idade;
+			public double peso;
+			
+			
+			private void Anda() {
+				System.out.println("Estou andando..." + idade);
+			}
+			
+
+}
---
+++ Módulo 1 - 120h/1. Lógica de programação com java - 30h/Introdução a linguagem Java/05-introducao-poo/src/application/program/MainAula04.java
@@ -0,0 +1,18 @@
+package application.program;
+
+import application.Aula04POO_ModificadoresDeAcesso;
+
+public class MainAula04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Aula04POO_ModificadoresDeAcesso exemplo = new Aula04POO_ModificadoresDeAcesso();
+		
+		// não consigo acessar pois o método está privado.
+		/*
+		exemplo.Anda();
+		*/
+	}
+
+}
