6b585a1e3fc22c3282908f84ab463d517cc8805e
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-10-01 00:22:02 -0300

    Intermediary Front-end: If, switch, for, while -> Praticando uso do switch

---
+++ Módulo 1 - 120h/4. Intermediary Front-End - 30h/06 - If, switch, for, while/4 - Praticando uso do switch/README.md
@@ -0,0 +1,6 @@
+# PRATICANDO USO DO SWITCH
+
+✅ Ele substitui muito bem o else if
+
+
+✅ Se não colocar o break, o switch vai executar todos os “cases”.
---
+++ Módulo 1 - 120h/4. Intermediary Front-End - 30h/06 - If, switch, for, while/4 - Praticando uso do switch/praticandoswitch.js
@@ -0,0 +1,17 @@
+var diaSem = 3
+
+switch (diaSem) {
+    case 1:
+        console.log("Domingo")
+        break
+
+    case 2:
+        console.log("Segunda")
+        break
+
+    case 3:
+        console.log("Terça")
+        break
+
+    default: console.log("Esse dia não existe mais")
+}
