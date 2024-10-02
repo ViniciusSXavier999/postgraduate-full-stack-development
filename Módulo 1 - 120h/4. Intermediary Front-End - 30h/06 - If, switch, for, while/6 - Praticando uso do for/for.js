bd134f37eefabca8ce9cfe8e791383b175a46e1f
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-10-01 22:45:55 -0300

    Intermediary Front-end: If, switch, for, while -> Praticando uso do for

---
+++ Módulo 1 - 120h/4. Intermediary Front-End - 30h/06 - If, switch, for, while/6 - Praticando uso do for/for.js
@@ -0,0 +1,26 @@
+// for in
+
+// CRIAÇÃO DE OBJETO DE FORMA LITERAL
+var carro = [{ modelo: 'Audio A3', marca: 'Audi', ano: 2020 },
+{ modelo: 'x80', marca: 'Ferrari', ano: 2029 }]
+
+
+/*
+ 
+for (let i in carro){
+console.log(carro[i])
+}
+ 
+*/
+
+
+// UTILIZANDO O FOR
+for (var i = 0; i < carro.length; i++) {
+     let result = carro[i]
+
+    // buscando somente a marca
+    console.log(result.marca)
+
+    // buscando somente o modelo
+    console.log(result.modelo)
+}
